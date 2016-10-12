3c23ea16fe96d1f223f41e16bf13f8034751e391
MingLeo <liumingvic@gmail.com>
2016-10-12 23:40:42 +1100

    add unit test

--- UnitTestAFX/UnitTest1.cs
+++ UnitTestAFX/UnitTest1.cs
@@ -1,16 +1,60 @@
 ﻿using System;
 using NUnit.Framework;
+using MyGame;
+using SwinGameSDK;
 
 namespace UnitTestProject1
 {
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        public void TestMethod1()
-        {
-            Assert.IsNotNull(100);
+        Player p = new Player(10, 10, 10, 10);
+        EnemyCircular ec = new EnemyCircular(20, 20, 20, 20);
+        EnemyLinear el = new EnemyLinear(30, 30, 30, 30);
 
+        [Test]
+        public void Existence_Player()
+        {
+            Assert.IsNotNull(p);
+        }
+
+        [Test]
+        public void Existence_EnemyCircular()
+        {
+            Assert.IsNotNull(ec);
+        }
+
+        [Test]
+        public void Existence_EnemyLinear()
+        {
+            Assert.IsNotNull(el);
+        }
+
+        [Test]
+        public void Initiation_Player()
+        {
+            Assert.AreEqual(p.XLocation, 10);
+            Assert.AreEqual(p.YLocation, 10);
+            Assert.AreEqual(p.Speed, 10);
+            Assert.AreEqual(p.Hp, 10);
+        }
+
+        [Test]
+        public void Initiation_EnemyCircular()
+        {
+            Assert.AreEqual(ec.XLocation, 20);
+            Assert.AreEqual(ec.YLocation, 20);
+            Assert.AreEqual(ec.Speed, 20);
+            Assert.AreEqual(ec.Hp, 20);
+        }
+
+        [Test]
+        public void Initiation_EnemyLinear()
+        {
+            Assert.AreEqual(el.XLocation, 30);
+            Assert.AreEqual(el.YLocation, 30);
+            Assert.AreEqual(el.Speed, 30);
+            Assert.AreEqual(el.Hp, 30);
         }
     }
 }
