423082138b507225b6af35db936d46a2c9a09b4b
Ming_Liu_100857112 <liumingvic@gmail.com>
2016-10-25 00:24:19 +1100

    Refactor IPatternCurve: MovePattern

--- src/IPatternCurve.cs
+++ src/IPatternCurve.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 namespace MyGame
 {
+    /// <summary>
+    /// Circular move pattern
+    /// </summary>
 	public interface IPatternCurve
-	{
-		void MovePattern (double aCentreX, double aCentreY, double aRadiusX, double aRadiusY, int directionX, int directionY);
-	}
+    {
+        void MovePattern(double aCentreX, double aCentreY, double aRadiusX, double aRadiusY, int directionX, int directionY);
+    }
 }
 
